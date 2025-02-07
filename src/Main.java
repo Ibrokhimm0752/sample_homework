81f0b44f262307229856ac7bedf1c4b4c45b1ca8
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-02-07 11:32:19 -0500

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
+    int x =5;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!" + x);
     }
 }
